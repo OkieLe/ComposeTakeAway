6a4d922efd9e692d4e239883ac20f8837c2ac345
Le Xing <xingled@gmail.com>
2021-11-28 22:29:42 +0800

    [Le] refactor DataResult

--- app/src/main/java/com/example/takeaway/data/model/DataResult.kt
+++ app/src/main/java/com/example/takeaway/data/model/DataResult.kt
@@ -4,28 +4,60 @@ import retrofit2.HttpException
 import timber.log.Timber
 import java.io.IOException
 import java.lang.Exception
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 
-sealed class DataResult<T> {
-    open fun get(): T? = null
-
-    fun isSuccess(): Boolean = this is Success
-}
+sealed class DataResult<T>
 
 data class Success<T>(val data: T) : DataResult<T>()
+data class Error<T>(val type: ErrorType, val message: String? = "") : DataResult<T>()
 
-data class Error<T>(val throwable: Throwable, val isNetworkError: Boolean = false) : DataResult<T>()
+sealed interface ErrorType {
+    object NetworkError : ErrorType
+    object ServerError : ErrorType
+}
+
+@OptIn(ExperimentalContracts::class)
+fun <T> DataResult<T>.isSuccess(): Boolean {
+    contract {
+        returns(true) implies (this@isSuccess is Success)
+    }
+    return this is Success
+}
+
+fun <T> DataResult<T>.onSuccess(block: (T) -> Unit): DataResult<T> {
+    if (isSuccess()) {
+        block(data)
+    }
+    return this
+}
+
+@OptIn(ExperimentalContracts::class)
+fun <T> DataResult<T>.isError(): Boolean {
+    contract {
+        returns(true) implies (this@isError is Error)
+    }
+    return this is Error
+}
+
+fun <T> DataResult<T>.onError(block: (ErrorType, String?) -> Unit): DataResult<T> {
+    if (isError()) {
+        block(type, message)
+    }
+    return this
+}
 
 inline fun <T> wrapDataResult(dataFetcher: () -> T): DataResult<T> {
     return try {
         Success(dataFetcher.invoke())
     } catch (e: IOException) {
         Timber.d("Network error ${e.message}")
-        Error(e, true)
+        Error(ErrorType.NetworkError)
     } catch (e: HttpException) {
         Timber.d("Server error ${e.message}")
-        Error(e)
+        Error(ErrorType.ServerError, e.message)
     } catch (e: Exception) {
         Timber.d("Unknown error ${e.message}")
-        Error(e)
+        Error(ErrorType.ServerError, e.message)
     }
 }
