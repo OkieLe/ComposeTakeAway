1b026e2eb59386cf11c91923e3040bb49bede9d4
Le Xing <xingled@gmail.com>
2022-05-02 14:14:44 +0800

    [Le] message on about page

--- app/src/main/java/com/example/takeaway/about/AboutScreen.kt
+++ app/src/main/java/com/example/takeaway/about/AboutScreen.kt
@@ -2,18 +2,27 @@ package com.example.takeaway.about
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.takeaway.R
 
 @Composable
 fun AboutScreen() {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = stringResource(id = R.string.about_label))
+        Text(
+            modifier = Modifier.padding(horizontal = 24.dp),
+            textAlign = TextAlign.Center,
+            text = stringResource(id = R.string.about_message),
+            style = MaterialTheme.typography.h6
+        )
     }
 }
 
